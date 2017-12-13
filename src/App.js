3cdea6b32c17dcf4fe17785c855d45b9f62d24c2
Isaac Cook <isaac@simpload.com>
2017-12-13 16:57:33 -0600

    Login workingish/User profile page
    * Wiring for setting payout address almost done

--- src/App.js
+++ src/App.js
@@ -2,18 +2,113 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Redirect
 } from 'react-router-dom'
-import LinkedStateMixin from 'react-addons-linked-state-mixin';
 import "../node_modules/jquery/dist/jquery.js";
 import "../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js";
 import "../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss";
 import axios from 'axios';
 import './App.scss';
 
-const Home = () => (
+const Credits = () => (
   <div>
-    <h2>Home</h2>
+    <h2>Credits</h2>
+  </div>
+)
+
+class PayoutAddress extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+			newVal: this.props.address
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(){
+		console.log(this.state.newVal)
+		console.log(this.props.currency)
+  }
+	render() {
+		return (
+			<tr>
+				<th scope="row">{this.props.currency}</th>
+				<td>
+					<div className="input-group">
+						<input type="text" value={this.props.address} className="form-control"
+							value={this.state.newVal}
+							onChange={(event) => this.setState({newVal: event.target.value})}/>
+						<span className="input-group-btn">
+							<button className="btn btn-info" type="button" onClick={() => this.handleSubmit()} >
+								Set</button>
+						</span>
+					</div>
+				</td>
+			</tr>
+		)
+	}
+}
+class Profile extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+			user: {},
+			payoutAddrs: {},
+    };
+    var instance = axios.create({
+			baseURL: 'http://localhost:3000/v1/',
+			timeout: 1000,
+			headers: {'Authorization': 'Bearer ' + this.props.token}
+		});
+    instance.get("user/me")
+			.then(res => {
+				var resp = res.data.data
+        this.setState({
+					user: resp.user,
+					payoutAddrs: resp["payout_addresses"]
+				})
+			}).catch(error => {
+					this.setState({error: error.response.data.errors[0].title})
+			});
+  }
+  render() {
+		var addrs = this.state.payoutAddrs
+		var rows = Object.keys(addrs).map((key) => (
+			<PayoutAddress currency={key} address={addrs[key]} />))
+    return (
+      <div className="container">    
+        <h2>Profile</h2>
+        <table className="table table-striped">
+					<tbody>
+          <tr>
+            <th scope="row">Username</th>
+            <td>{ this.state.user.username }</td>
+          </tr>
+          <tr>
+            <th scope="row">Email</th>
+            <td>{ this.state.user.email }</td>
+          </tr>
+				</tbody>
+        </table>
+        <h2>Payout Addresses</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Currency</th>
+              <th scope="col">Payout Address</th>
+            </tr>
+          </thead>
+          <tbody>
+						{rows}
+          </tbody>
+        </table>
+      </div>
+    )}
+}
+
+const Blocks = () => (
+  <div>
+    <h2>Recent Blocks</h2>
   </div>
 )
 
@@ -24,14 +119,17 @@ class Error extends Component {
 }
 
 class Login extends Component {
-  mixins: [LinkedStateMixin]
   constructor(props){
     super(props);
     this.state = {
       username:'',
       password:'',
-      error:''
+      error:'',
+      redirectToReferrer: false
     };
+    if (this.props.isAuthenticated) {
+        this.setState({ redirectToReferrer: true })
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleSubmit(event){
@@ -40,11 +138,21 @@ class Login extends Component {
 			.then(res => {
 				var resp = res.data.data
 				this.props.login(resp.username, resp.user_id, resp.token);
+        this.setState({ redirectToReferrer: true })
 			}).catch(error => {
 					this.setState({error: error.response.data.errors[0].title})
 			});
   }
   render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    const { redirectToReferrer } = this.state
+    
+    if (redirectToReferrer) {
+      return (
+        <Redirect to={from}/>
+      )
+    }
+    
     return (
       <div className="container">    
         <div id="loginbox" style={{marginTop: 50}} className="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
@@ -100,7 +208,6 @@ class Login extends Component {
 }
 
 class App extends Component {
-  mixins: [LinkedStateMixin]
   constructor(props){
     super(props);
     this.state = {
@@ -109,6 +216,15 @@ class App extends Component {
       user_id:'',
       token:'',
     };
+    var user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      this.state = {
+        loggedIn: true,
+        username: user.username,
+        userId: user.userId,
+        token: user.token,
+      }
+    }
     this.login = this.login.bind(this);
   }
   login(username, userId, token){
@@ -118,6 +234,8 @@ class App extends Component {
 			userId: userId,
 			token: token,
     })
+    localStorage.setItem('user', JSON.stringify({
+      username: username, userId: userId, token: token}));
   }
   render() {
     return (
@@ -137,19 +255,26 @@ class App extends Component {
 
 							<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 								<ul className="nav navbar-nav">
-									<li><Link to="/">Home</Link></li>
-									{ this.state.loggedIn &&
-										<li><Link to="/addrs">Payout Addresses</Link></li>
-									}
-									<li><Link to="/login">Login</Link></li>
+									<li><Link to="/">Blocks</Link></li>
+									{ this.state.loggedIn && <li><Link to="/credits">Credits</Link></li>}
+									{ this.state.loggedIn && <li><Link to="/profile">Profile</Link></li> }
+									{ !this.state.loggedIn &&
+                    <li><Link to="/login">Login</Link></li>
+                  }
 								</ul>
 							</div>
 						</div>
 					</nav>
-					<Route path="/login" render={props => (
-						<Login {...props} login={this.login} />
+					<Route path="/credits" authed={this.state.loggedIn} render={props => (
+						<Credits {...props} />
 					)}/>
-					<Route exact path="/" component={Home}/>
+					<Route path="/profile" authed={this.state.loggedIn} render={props => (
+						<Profile {...props} token={this.state.token}/>
+					)}/>
+					<Route path="/login" render={props => (
+						<Login {...props} login={this.login} authed={this.state.loggedIn} />
+					)}/>
+					<Route exact path="/" component={Blocks}/>
 				</div>
 			</Router>
 		)
