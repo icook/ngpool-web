3f535932605ed43b1d30f15499a8d9dd5e305b57
Isaac Cook <isaac@simpload.com>
2017-12-23 15:42:18 -0600

    Setup a block details view

--- src/Blocks.js
+++ src/Blocks.js
@@ -1,4 +1,68 @@
 import React, { Component } from 'react';
+import TimeAgo from 'react-timeago'
+
+class BlockRow extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+			extra: null,
+    }
+    this.load = this.load.bind(this);
+  }
+  load () {
+    if (this.state.extra) {
+      this.setState({extra: null})
+      return
+    }
+    this.props.axios.get("block/" + this.props.block.hash)
+			.then(res => {
+        this.setState({extra: res.data.data.block})
+			}).catch(error => {
+        console.log(error)
+			});
+  }
+  render() {
+    var block = this.props.block;
+    var extra = '';
+    if (this.state.extra) {
+      var det = this.state.extra
+      extra = (
+			<tr>
+				<td colSpan="7">
+          <h4>Block Details</h4>
+          <table className="table table-striped">
+            <tbody>
+            <tr>
+              <th scope="col">Hash</th>
+              <td><code>{block.hash}</code></td>
+            </tr>
+            <tr>
+              <th scope="col">PoW Hash</th>
+              <td><code>{det.powhash}</code></td>
+            </tr>
+            <tr>
+              <th scope="col">Payout Data</th>
+              <td><code>{JSON.stringify(det.payout_data, null, 2)}</code></td>
+            </tr>
+            </tbody>
+          </table>
+        </td>
+			</tr>)
+    }
+    return ([
+      <tr onClick={this.load} style={{cursor: 'pointer'}}>
+        <td><i className="glyphicon glyphicon-plus" /></td>
+        <td>{block.currency}</td>
+        <td>{block.height}</td>
+        <td>{block.difficulty}</td>
+        <td>{block.powalgo}</td>
+        <td><TimeAgo date={block.mined_at} /></td>
+        <td>{block.status}</td>
+      </tr>,
+      extra
+    ])
+  }
+}
 
 class Blocks extends Component {
   constructor(props){
@@ -18,22 +82,20 @@ class Blocks extends Component {
   render() {
     var blocks = this.state.blocks
 		var rows = Object.keys(blocks).map((key) => (
-			<tr>
-				<td>{blocks[key].currency}</td>
-				<td>{blocks[key].hash}</td>
-				<td>{blocks[key].powalgo}</td>
-				<td>{blocks[key].mined_at}</td>
-			</tr>))
+			<BlockRow axios={this.props.axios} block={blocks[key]} />))
     return (
       <div className="container">    
         <h2>Recent Blocks</h2>
         <table className="table table-striped">
           <thead>
             <tr>
+              <th scope="col"></th>
               <th scope="col">Currency</th>
-              <th scope="col">Hash</th>
+              <th scope="col">Height</th>
+              <th scope="col">Difficulty</th>
               <th scope="col">Algo</th>
               <th scope="col">Time</th>
+              <th scope="col">Status</th>
             </tr>
           </thead>
           <tbody>
