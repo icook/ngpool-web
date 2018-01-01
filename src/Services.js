dbd6d9b6c6a9a7df9c41fc7fb7f5c5a944fb04d1
Isaac Cook <isaac@simpload.com>
2018-01-01 16:01:50 -0600

    Add stratums to Service Status view

--- src/Services.js
+++ src/Services.js
@@ -3,32 +3,11 @@ import MDSpinner from 'react-md-spinner';
 import TimeAgo from 'react-timeago'
 import ReactJson from 'react-json-view'
 
-class Services extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      services: [],
-      loading: false,
-    }
-    this.load = this.load.bind(this);
-  }
-  load() {
-    this.setState({loading: true}, () => {
-      this.props.axios.get("coinservers").then(res => {
-          this.setState({services: Object.values(res.data.data.coinservers), loading: false})
-        }).catch(error => {
-          console.log(error)
-          this.setState({loading: false})
-        })
-    })
-  }
-  componentDidMount() {
-    this.load()
-  }
+class ServiceTable extends Component {
   render() {
-    var services = this.state.services
+    var services = this.props.services
     var rows
-    if (this.state.loading) {
+    if (this.props.loading) {
       rows = (<tr><td colSpan="8" className="jumbotron text-center">
         <div className="jumbotron"><MDSpinner size={50}/></div>
       </td></tr>)
@@ -44,21 +23,59 @@ class Services extends Component {
         </tr>)})
     }
     return (
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Status</th>
+            <th scope="col">Labels</th>
+            <th scope="col">Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+      )
+  }
+}
+
+class Services extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      stratums: [],
+      coinservers: [],
+      loading: false,
+    }
+    this.load = this.load.bind(this);
+  }
+  load() {
+    this.setState({loading: true}, () => {
+      this.props.axios.get("services").then(res => {
+        var data = res.data.data
+        this.setState({
+          coinservers: Object.values(data.coinservers),
+          stratums: Object.values(data.stratums),
+          loading: false,
+        })
+      }).catch(error => {
+        console.log(error)
+        this.setState({loading: false})
+      })
+    })
+  }
+  componentDidMount() {
+    this.load()
+  }
+  render() {
+    var {loading, stratums, coinservers} = this.state
+    return (
       <div className="container">    
-        <h2>Service Status</h2>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Status</th>
-              <th scope="col">Labels</th>
-              <th scope="col">Updated</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rows}
-          </tbody>
-        </table>
+        <h2>Stratums</h2>
+        <ServiceTable services={stratums} loading={loading}/>
+        <h2>Coinservers</h2>
+        <ServiceTable services={coinservers} loading={loading}/>
       </div>
       )
   }
