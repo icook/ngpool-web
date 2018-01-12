83ba3b287229abc3f119d2e27ea64fc4b413796c
Isaac Cook <isaac@simpload.com>
2018-01-11 21:50:51 -0600

    Abstract the hashrate graph to its own component

--- src/Workers.js
+++ src/Workers.js
@@ -4,6 +4,67 @@ import MDSpinner from 'react-md-spinner';
 import { ReferenceLine, Tooltip, YAxis, XAxis, ResponsiveContainer, LineChart, Line } from 'recharts';
 import moment from 'moment';
 
+var yScale = (val) => {
+  if (val > 1e12)
+    return val / 10e12 + " Thps"
+  if (val > 1e9)
+    return val / 10e9 + " Ghps"
+  if (val > 1e6)
+    return val / 10e6 + " Mhps"
+  if (val > 1e3)
+    return val / 10e3 + " Khps"
+  return val + " hps"
+}
+
+class HashrateGraph extends Component {
+  render() {
+    var times = {}
+    var timeFormat = 'ddd HH:mm'
+    var end = moment().startOf('minute')
+    var start = end.subtract(1, 'hours')
+    var hashrateTotal = 0
+    for (var slice of this.props.slices) {
+      var m = moment(slice.minute)
+      if (slice < start || slice > end)
+        continue;
+      slice.axis = m.format(timeFormat)
+      slice.unix = m.unix()
+      hashrateTotal += slice.hashrate
+      times[slice.unix] = slice
+    }
+    for (var i = 0; i < 60; i++) {
+      if (!(start.unix() in times)) {
+        times[start.unix()] = {unix: start.unix(), hashrate: 0, axis: start.format(timeFormat)}
+      }
+      start.add(1, 'minute')
+    }
+    var minute_shares = Object.values(times)
+    var average_hashrate = hashrateTotal / minute_shares.length
+    minute_shares.sort((a, b) => a.unix - b.unix)
+
+    return (
+      <ResponsiveContainer width="100%" height={100}>
+        <LineChart data={minute_shares}>
+          <YAxis tickFormatter={yScale}/>
+          <XAxis dataKey="axis" />
+          <Line animationDuration={500} dot={false} type="monotone" dataKey="hashrate" stroke="#8884d8" />
+          <ReferenceLine y={average_hashrate} label="Average"/>
+          <Tooltip formatter={(val, label, meta) => {
+            if (!meta.payload.stratum)
+            return "no data"
+            return (
+            <span>
+              { meta.payload.hashrate }<br/>
+              difficulty: {Math.round(meta.payload.difficulty / meta.payload.shares * 100) / 100}<br/>
+              stratum: {meta.payload.stratum}<br/>
+              sharechain: {meta.payload.sharechain}
+            </span>
+            )}}/>
+        </LineChart>
+      </ResponsiveContainer>)
+  }
+}
+
 class Workers extends Component {
   constructor(props){
     super(props);
@@ -35,17 +96,6 @@ class Workers extends Component {
   componentDidMount() {
     this.load()
   }
-  yScale(val) {
-    if (val > 1e12)
-      return val / 10e12 + " Thps"
-    if (val > 1e9)
-      return val / 10e9 + " Ghps"
-    if (val > 1e6)
-      return val / 10e6 + " Mhps"
-    if (val > 1e3)
-      return val / 10e3 + " Khps"
-    return val + " hps"
-  }
   render() {
     var workers = {};
     for (var workerName of Object.keys(this.state.workers)) {
@@ -64,29 +114,7 @@ class Workers extends Component {
           online: false,
         }
       }
-      var times = {}
-      var timeFormat = 'ddd HH:mm'
-      var end = moment().startOf('minute')
-      var start = end.subtract(1, 'hours')
-      var hashrateTotal = 0
-      for (var slice of slices) {
-        var m = moment(slice.minute)
-        if (slice < start || slice > end)
-          continue;
-        slice.axis = m.format(timeFormat)
-        slice.unix = m.unix()
-        hashrateTotal += slice.hashrate
-        times[slice.unix] = slice
-      }
-      for (var i = 0; i < 60; i++) {
-        if (!(start.unix() in times)) {
-          times[start.unix()] = {unix: start.unix(), hashrate: 0, axis: start.format(timeFormat)}
-        }
-        start.add(1, 'minute')
-      }
-      workers[workerName].minute_shares = Object.values(times)
-      workers[workerName].average_hashrate = hashrateTotal / workers[workerName].minute_shares.length
-      workers[workerName].minute_shares.sort((a, b) => a.unix - b.unix)
+      workers[workerName].slices = slices
     }
     var rows
     if (this.state.loadingWorkers || this.state.loadingMinuteShares) {
@@ -102,32 +130,13 @@ class Workers extends Component {
           <td>{Math.round(worker.difficulty * 100, 2) / 100}</td>
           <td>{worker.online ? "Active" : "Inactive"}</td>
         </tr>)]
-        if (worker.minute_shares) {
-          rows.push((
+        if (worker.slices) {
+          rows.push(
             <tr key={name + "-graph"}>
               <td colSpan="4">
-                <ResponsiveContainer width="100%" height={100}>
-                  <LineChart data={worker.minute_shares}>
-                    <YAxis tickFormatter={this.yScale}/>
-                    <XAxis dataKey="axis" />
-                    <Line animationDuration={500} dot={false} type="monotone" dataKey="hashrate" stroke="#8884d8" />
-                    <ReferenceLine y={worker.average_hashrate} label="Average"/>
-                    <Tooltip formatter={(val, label, meta) => {
-                      if (!meta.payload.stratum)
-                        return "no data"
-                      return (
-                        <span>
-                          { meta.payload.hashrate }<br/>
-                          difficulty: {Math.round(meta.payload.difficulty / meta.payload.shares * 100) / 100}<br/>
-                          stratum: {meta.payload.stratum}<br/>
-                          sharechain: {meta.payload.sharechain}
-                        </span>
-                      )}}/>
-                  </LineChart>
-                </ResponsiveContainer>
+                <HashrateGraph slices={worker.slices}/>
               </td>
-            </tr>
-          ))
+            </tr>)
         }
         return rows
       })
