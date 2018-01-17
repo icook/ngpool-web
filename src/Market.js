2e02f73986bc839129d59525af08ebf876421c7f
Isaac Cook <isaac@simpload.com>
2018-01-16 18:14:45 -0600

    Tweaks and fixes to liquidity view

--- src/Market.js
+++ src/Market.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Redirect} from 'react-router-dom'
+import num from 'num'
 
 class Market extends Component {
   constructor(props){
@@ -11,6 +12,7 @@ class Market extends Component {
 			buyLiquidity: {},
 			trades: [],
     };
+    this.firstLoad = true;
     this.websocket = null;
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -36,33 +38,40 @@ class Market extends Component {
   }
   msg(msg) {
     var dat = JSON.parse(msg.data);
-    console.log(dat)
     this.setState((prevState, props) => {
       var {sellLiquidity, buyLiquidity} = prevState
       for (var priceRaw in dat.blu) { 
-        var price = Number(priceRaw);
-        var change = Number(dat.blu[priceRaw]);
-        if (buyLiquidity[price] === undefined) {
-          buyLiquidity[price] = Number(0)
+        var price = num(priceRaw);
+        var change = num(dat.blu[priceRaw]);
+        if (buyLiquidity[priceRaw] === undefined) {
+          buyLiquidity[priceRaw] = {price: price, amount: num(0)}
         }
-        buyLiquidity[price] = change + buyLiquidity[price]
-        if (buyLiquidity[price] == 0) {
-          delete buyLiquidity[price];
+        buyLiquidity[priceRaw].amount = change.add(buyLiquidity[price].amount)
+        if (buyLiquidity[priceRaw].amount == 0) {
+          delete buyLiquidity[priceRaw];
         }
       }
       for (priceRaw in dat.slu) { 
-        price = Number(priceRaw);
-        change = Number(dat.slu[priceRaw]);
-        if (sellLiquidity[price] === undefined) {
-          sellLiquidity[price] = Number(0)
+        price = num(priceRaw);
+        change = num(dat.slu[priceRaw]);
+        if (sellLiquidity[priceRaw] === undefined) {
+          sellLiquidity[priceRaw] = {price: price, amount: num(0)}
         }
-        sellLiquidity[price] = change + sellLiquidity[price]
-        if (sellLiquidity[price] == 0) {
-          delete sellLiquidity[price];
+        sellLiquidity[priceRaw].amount = change.add(sellLiquidity[priceRaw].amount)
+        if (sellLiquidity[priceRaw].amount == 0) {
+          delete sellLiquidity[priceRaw];
         }
       }
       return {sellLiquidity: sellLiquidity, buyLiquidity: buyLiquidity}
-    })
+    }, () => {
+      // Only do this when loading initial data
+      if (this.firstLoad) {
+        this.refs["spread"].scrollIntoView(
+          {behavior: "instant", block: "center", inline: "center"})
+        this.firstLoad = false;
+      }
+      }
+    )
   }
   componentDidMount() {
     var channel = "market_" + this.props.match.params.id;
@@ -85,30 +94,38 @@ class Market extends Component {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <table className="table table-liquidity">
-              <tbody className="buy">
-                { Object.keys(buyLiquidity).sort().slice(-25, -1).map((price) => (
-                <tr key={price}>
-                  <td>{Number(price).toFixed(8)}</td>
-                  <td>{buyLiquidity[price]}</td>
-                </tr>
-                ))}
-              </tbody>
+            <h3>Orderbook</h3>
+            <table className="table" style={{marginBottom: 0}}>
               <thead>
                 <tr>
                   <th>Price</th>
                   <th>Amount</th>
                 </tr>
               </thead>
-              <tbody className="sell">
-                { Object.keys(sellLiquidity).sort().slice(-25, -1).map((price) => (
-                <tr key={price}>
-                  <td>{Number(price).toFixed(8)}</td>
-                  <td>{sellLiquidity[price]}</td>
-                </tr>
-                ))}
-              </tbody>
             </table>
+            <div className="liquidity-container">
+              <table className="table table-liquidity">
+                <tbody className="buy">
+                  { Object.keys(buyLiquidity).sort().slice(-25, -1).map((price) => (
+                  <tr key={price}>
+                    <td>{buyLiquidity[price].price.toString()}</td>
+                    <td>{buyLiquidity[price].amount.toString()}</td>
+                  </tr>
+                  ))}
+                </tbody>
+                <tr>
+                  <td colSpan="2" ref="spread">Spread</td>
+                </tr>
+                <tbody className="sell">
+                  { Object.keys(sellLiquidity).sort().slice(-25, -1).map((price) => (
+                  <tr key={price}>
+                    <td>{sellLiquidity[price].price.toString()}</td>
+                    <td>{sellLiquidity[price].amount.toString()}</td>
+                  </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
